787174e89ae8ec3c8bfc97f6a5e42f606b113124
wspspring <wspspring@126.com>
2020-08-03 17:38:13 +0800

    login and logout

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+    <!-- 退出按钮 -->
+    <el-button type="info" @click="logout">退出</el-button>
+</div>
+</template>
+
+<script>
+export default {
+  methods: {
+    //   退出登录
+    logout () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+<style lang='less' scoped>
+
+</style>
